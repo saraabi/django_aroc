5061a2ca96dd188ae020feb8ccd47862c0849a07
Samer <samer@kindlingllc.com>
2022-11-12 15:42:35 -0800

    add production settings

---
+++ django_aroc/settings/production.py
@@ -0,0 +1,12 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['aroc.herokuapp.com', 'araborganizing.org']
+
+# SECURE_SSL_REDIRECT = True
+
+# STATIC_URL = 'https://{0}/{1}/'.format(
+#     AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
