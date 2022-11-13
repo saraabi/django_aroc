c311183204aa1b0622ad6db2c781c762e7b6b1a8
Samer <samer@kindlingllc.com>
2022-11-13 09:37:25 -0800

    add about page

--- aroc/urls.py
+++ aroc/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import Home
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('about', TemplateView.as_view(template_name='about.html'), name='about'),
 ]
