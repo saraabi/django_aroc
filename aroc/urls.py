26ff807bf507a76d7ba4c9ca89def433d157da03
Samer <samer@kindlingllc.com>
2022-11-13 20:15:20 -0800

    add learn page

--- aroc/urls.py
+++ aroc/urls.py
@@ -6,6 +6,9 @@ from .views import Home
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('about', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('about', TemplateView.as_view(
+        template_name='about.html'), name='about'),
+    path('learn', TemplateView.as_view(
+        template_name='learn.html'), name='learn'),
     # path('subscriber', SubscriberCreate.as_view(), name='subscriber_create'),
 ]
